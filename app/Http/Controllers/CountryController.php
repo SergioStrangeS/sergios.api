a50cee6856701100f92306538d1f27c0261cb8e2
Sergios <xabarov-05@mail.ru>
2021-05-09 16:35:57 +0300

    Country@Index && Country@Store

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function index()
+    {
+        return Country::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Country::create($request->all());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/', function() {
-   return "Hello world!";
-});
+Route::get('/country', [CountryController::class, 'index'] );
+Route::post('/country', [CountryController::class, 'store']);
